272511bd7d21da568be4fdc605f1746491344a09
Vila Conesa, Ferran <Ferran.Vila@gft.com>
2016-11-29 05:31:00 +0100

    update lookpback sources

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
-require('newrelic');
+if (process.env.NEW_RELIC_TOKEN) {
+  require('newrelic');
+}
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 
@@ -10,12 +12,12 @@ app.start = function () {
   // start the web server
   return app.listen(function () {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
+    //var baseUrl = app.get('url').replace(/\/$/, '');
     //console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
+    //if (app.get('loopback-component-explorer')) {
+    //var explorerPath = app.get('loopback-component-explorer').mountPath;
+    //console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    //}
   });
 };
 
