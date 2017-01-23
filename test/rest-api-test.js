42b8e84ca3c79abea4e6ad3a2950f024e01d3cfc
Vila Conesa, Ferran <Ferran.Vila@gft.com>
2017-01-23 11:24:09 +0100

    re-enable tests

--- test/rest-api-test.js
+++ test/rest-api-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app = require('../server/server');
-// var request = require('supertest');
+var request = require('supertest');
 
 describe('REST API request', function () {
   before(function (done) {
@@ -15,59 +15,50 @@ describe('REST API request', function () {
     done();
   });
 
-  // it('should get the list of schools', function (done) {
-  //   request(app)
-  //     .get('/api/schools')
-  //     .set('Content-Type', 'application/json')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(200, done);
-  // });
+  it('should get the list of schools', function (done) {
+    request(app)
+      .get('/api/schools')
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
 
-  // it('should fail getting my school without authentication', function (done) {
-  //   request(app)
-  //     .get('/api/schools/1')
-  //     .set('Content-Type', 'application/json')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(401, done);
-  // });
+  it('should fail getting my school without authentication', function (done) {
+    request(app)
+      .get('/api/schools/1')
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
 
-  // it('should fail getting my account without authentication', function (done) {
-  //   request(app)
-  //     .get('/api/students/1')
-  //     .set('Content-Type', 'application/json')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(401, done);
-  // });
+  it('should fail getting my account without authentication', function (done) {
+    request(app)
+      .get('/api/students/1')
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401, done);
+  });
 
-  // it('should fail getting my roles without authentication', function (done) {
-  //   request(app)
-  //     .get('/api/students/1/roles')
-  //     .set('Content-Type', 'application/json')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(401, done);
-  // });
-
-  // it('should fail getting my accessTokens without authentication', function (done) {
-  //   request(app)
-  //     .get('/api/students/1/accessTokens')
-  //     .set('Content-Type', 'application/json')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(401, done);
-  // });
+  it('should fail getting my accessTokens without authentication', function (done) {
+    request(app)
+      .get('/api/students/1/accessTokens')
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401, done);
+  });
 });
 
 describe('Unexpected Usage', function () {
-  // it('should not crash the server when posting a bad id', function (done) {
-  //   request(app)
-  //     .get('/api/students/foobar')
-  //     .set('Content-Type', 'application/json')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(404, done);
-  // });
+  it('should not crash the server when posting a bad id', function (done) {
+    request(app)
+      .get('/api/foobar')
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+  });
 });
