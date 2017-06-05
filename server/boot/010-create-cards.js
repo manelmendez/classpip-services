55fc70e1c48cf9065981a0c1fbdcf5e7ffdad33b
unknown <maneeel93@gmail.com>
2017-06-05 16:27:25 +0200

    DB new models creation working

--- server/boot/010-create-cards.js
+++ server/boot/010-create-cards.js
@@ -6,156 +6,156 @@ module.exports = function (app, cb) {
     id: 1,
     name: 'Ter Stegen',
     image: 'img',
-    collectionId: 1,
     ratio: '0.20',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 3,
     name: 'Piqué',
     image: 'img',
-    collectionId: 1,
     ratio: '0.15',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 4,
     name: 'Rakitic',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 5,
     name: 'Busquets',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 6,
     name: 'Denis Suarez',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 7,
     name: 'Arda Turan',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 8,
     name: 'Iniesta',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 9,
     name: 'Luis Suarez',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 10,
     name: 'Messi',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 11,
     name: 'Neymar',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 12,
     name: 'Rafinha',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 13,
     name: 'Cillessen',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 14,
     name: 'Mascherano',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 17,
     name: 'Paco Alcácer',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 18,
     name: 'Jordi Alba',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 19,
     name: 'Digne',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 20,
     name: 'Sergi Roberto',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 21,
     name: 'André Gomes',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 22,
     name: 'Aleix Vidal',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 23,
     name: 'Umtiti',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 24,
     name: 'Mathieu',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }, {
     id: 25,
     name: 'Masip',
     image: 'img',
-    collectionId: 1,
     ratio: '0.30',
-    rank: 'gold'
+    rank: 'gold',
+    collectionId: 1
   }], function (err) {
     if (err) throw err;
     process.nextTick(cb);
